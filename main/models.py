e0b1a8a6fa4fd0742d69b3fd9df5a02ddae2f34c
Konstantin <konstantin_n.v@mail.ru>
2022-12-12 21:37:09 +0600

    add db

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Quiz(models.Model):
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=255, default=' ')
+    created_at = models.DateTimeField(auto_now_add=True)
+    times_taken = models.IntegerField(default=0, editable=False)
+
+    @property
+    def question_count(self):
+        return self.question_count()
+    
+    class Meta():
+        verbose_name_plural = "Quizzes"
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
+
+class Questions(models.Model):
+    quiz = models.ForeignKey(
+        Quiz,
+        related_name = "answers",
+        on_delete=models.DO_NOTHING
+    )
+class Answer(models.Model):
+    questions = models.ForeignKey(
+        Questions,
+        related_name='answers',
+        on_delete=models.DO_NOTHING
+    )
+    text = models.CharField(max_length=255)
+    correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+        
